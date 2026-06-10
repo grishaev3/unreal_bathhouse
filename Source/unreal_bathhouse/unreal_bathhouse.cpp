f5e7451e4351c0af6a2d66ca9381fae11245bb4f
Ilia Grishaev <grishaev3@gmail.com>
2026-06-10 15:13:59 +0300

    Добавил С++ код. Компилируется.

---
+++ Source/unreal_bathhouse/unreal_bathhouse.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "unreal_bathhouse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_bathhouse, "unreal_bathhouse" );
